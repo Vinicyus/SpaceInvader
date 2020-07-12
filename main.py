680a92008e0c6b4b0adf12437c2d6c27e15295e8
Vinicyus Anchieta <vinicyus_anchieta@hotmail.com>
2020-07-12 16:59:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import pygame
 import os
 import time
 import random
-pygame.font.init()
+from pygame import mixer
 
-WIDTH, HEIGHT = 750, 750
+pygame.font.init()
+pygame.init()
+mixer.init()
+
+WIDTH, HEIGHT = 750, 680
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (300,40) #screen center
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
 pygame.display.set_caption("Space Invader")
+programIcon = pygame.image.load(os.path.join("assets", "space_shuttle_icon.png"))
+pygame.display.set_icon(programIcon)
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
@@ -24,6 +32,133 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+# Music & Sound Effects
+gameOver = pygame.mixer.Sound("game_over.wav")
+shipExplosion = pygame.mixer.Sound("explosion2.wav")
+shipShoot = pygame.mixer.Sound("shoot.wav")
+
+# Color & Fonts
+white = (255, 255, 255)
+black = (0, 0, 0)
+grey = (122, 122, 122)
+red = (255, 0, 0)
+light_red = (255,153,153)
+green = (0, 204, 102)
+light_green = (204, 255, 153)
+blue = (0,0,255)
+light_blue = (153, 153, 255)
+yellow = (252,237,66)
+light_yellow = (255, 255, 178)
+
+extrasmallfont = pygame.font.SysFont("comicsansms", 15)
+smallfont = pygame.font.SysFont("comicsansms", 25)
+medfont = pygame.font.SysFont("comicsansms", 50)
+largefont = pygame.font.SysFont("comicsansms", 65)
+
+# Game Variables
+clock = pygame.time.Clock()
+FPS = 60
+
+def text_objects(text, color, size):
+	if size == "extrasmall":
+		textSurface = extrasmallfont.render(text, True, color)
+	elif size == "small":
+		textSurface = smallfont.render(text, True, color)
+	elif size == "medium":
+		textSurface = medfont.render(text, True, color)
+	elif size == "large":
+		textSurface = largefont.render(text, True, color)
+
+	return textSurface, textSurface.get_rect()
+
+def text_to_button(msg, color, buttonx, buttony, buttonwidth, butonheight, size="small"):
+	textSurf, textRect = text_objects(msg, color, size)
+	textRect.center = ((buttonx+(buttonwidth/2), buttony+(butonheight/2)))
+	WIN.blit(textSurf, textRect)
+
+def button(text, x, y, width, height, inactive_color, active_color, action = None):
+	cur = pygame.mouse.get_pos()
+	click = pygame.mouse.get_pressed()
+	if x + width > cur[0] > x and y + height > cur[1] > y:
+		pygame.draw.rect(WIN, active_color, (x, y, width, height))
+		if click[0] == 1 and action != None:
+			if action == "quit":
+				pygame.quit()
+				quit()
+			if action == "play":
+				main()
+			if action == "record":
+				records()
+	else:
+		pygame.draw.rect(WIN, inactive_color, (x, y, width, height))
+
+	text_to_button(text, black, x, y, width, height)
+
+def message_to_screen(msg,color, y_displace=0, size = "small"):
+	textSurf, textRect = text_objects(msg, color, size)
+	textRect.center = (WIDTH/2), (HEIGHT/2) + y_displace
+	WIN.blit(textSurf, textRect)
+
+def records():
+	record = True
+	file = open('scores.txt')
+	lines = sorted(file.readlines(), reverse = True, key = int)
+	while record:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_r:
+					main_menu()
+				elif event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+
+		WIN.blit(BG, (0,0))
+		message_to_screen("Records", yellow, -210, "large")
+		message_to_screen("Top 10 Records", white, -160, "extrasmall")
+
+		lineHeight = -130
+		i = 0
+		for line in lines:
+			line = line[:-1]
+			message_to_screen(str(i + 1) + "°: " + str(line), white, lineHeight, "small")
+			lineHeight += 30
+			i += 1
+			if i == 10:
+				break
+		file.close()
+
+		message_to_screen("Press R to return to the main menu", white, 210, "small")
+
+		pygame.display.update()
+		clock.tick(FPS)
+
+def pause():
+	paused = True
+	WIN.blit(BG, (0,0))
+	message_to_screen("Paused", yellow, -100, "large")
+	message_to_screen("Press R to return to the main menu", white, 0)
+	message_to_screen("Press C to continue", white, 30)
+	message_to_screen("Press Q to quit", white, 60)
+
+	while paused:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_c:
+					paused = False
+				elif event.key == pygame.K_r:
+					main_menu()
+				elif event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+		pygame.display.update()
+		clock.tick(FPS)
 
 class Laser:
 	def __init__(self, x, y, img):
@@ -144,55 +279,116 @@ def collide(obj1, obj2):
 	offset_y = obj2.y - obj1.y
 	return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None 
 
+def main_menu():
+	WIN.blit(BG, (0,0))
+	intro = True
+	pygame.mixer.music.load("menu_screen.mp3")
+	pygame.mixer.music.play(-1)
+	while intro:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+
+		message_to_screen("Space Invader", yellow, -100, "large")
+		message_to_screen("Protect the galaxy from foreign invaders!", white, -40, "small")
+
+		button("Play", 250,350,100,50, red, light_red, action="play" )
+		button("Records", 400,350,100,50, green, light_green, action="record")
+
+		pygame.display.update()
+		clock.tick(FPS)
+
 def main():
 	run = True
-	FPS = 60
-	level = 0
-	lives = 5
+	pygame.mixer.music.load("game_screen.mp3")
+	pygame.mixer.music.play(-1)
+	pygame.mixer.music.set_volume(0.3)
+	level = -10 #to start with 0
+	lives = 3
 	main_font = pygame.font.SysFont("comicsans", 50)
-	lost_font = pygame.font.SysFont("comicsans", 60)
 
 	enemies = []
 	wave_length = 5
 	enemy_vel =  1
 
-	player_vel = 5
-	laser_vel = 5
+	player_vel = 6
+	laser_vel = 7
 
-	player = Player(300, 630)
-
-	clock = pygame.time.Clock()
+	player = Player(300, 560)
 
 	lost = False
 	lost_count = 0
 
-	def redraw_window():
+	def redraw_window(lost):
 		WIN.blit(BG, (0,0))
-		#draw text
-		lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
-		level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+        # draw text
+		lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+		level_label = main_font.render(f"Score: {level}", 1, (255,255,255))
 
 		WIN.blit(lives_label, (10, 10))
-		WIN.blit(level_label, (WIDTH - level_label.get_width() -10, 10))
+		WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
 		for enemy in enemies:
-			enemy.draw(WIN)
+ 			enemy.draw(WIN)
 
 		player.draw(WIN)
 
-		if lost:
-			lost_label = lost_font.render("You Lost!!!", 1, (255,255,255))
-			WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
+		# if lost:
+		# 	lost_label = lost_font.render("You Lost!!", 1, (255,255,255))
+		# 	WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
 
-		pygame.display.update()		
+		# pygame.display.update()
+
+		if lost == True:
+			f = open('scores.txt', 'a')
+			f.writelines(str(level) + "\n")
+			f.close()
+			pygame.mixer.Sound.play(gameOver)
+
+		while lost == True:
+			WIN.blit(BG, (0,0))
+			message_to_screen("Game over", red, -120, size="large")
+			message_to_screen("Press R to return to the main menu", white, -50, size="small")
+			message_to_screen("Press C to play again", white, -20, size="small")
+			message_to_screen("Press Q to quit", white, 10, size="small")
+			pygame.display.update()
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					lost = False
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q:
+						lost = False
+					elif event.key == pygame.K_r:
+						main_menu()
+					if event.key == pygame.K_c:
+						main()
+
+		pygame.display.update()
 
 	while run:
 		clock.tick(FPS)	
-		redraw_window()
+		redraw_window(lost)
 
-		if lives < 0 or player.health <= 0:
-			lost = True
+		#if lives < 0 or player.health <= 0:
+		#if lives < 1:
+		#	lost = True
+		#	lost_count += 1
+
+		if player.health <= 0:
+			pygame.mixer.Sound.play(shipExplosion)
+			lives -= 1
 			lost_count += 1
+			if lives < 1:
+				lost = True
+				lost_count += 1
+			else:
+				player = Player(300, 560)
+
 
 		if lost:
 			if lost_count > FPS * 3:
@@ -201,7 +397,7 @@ def main():
 				continue
 
 		if len(enemies) == 0:
-			level += 1
+			level += 10
 			wave_length += 5
 			for i in range(wave_length):
 				enemy = Enemy(random.randrange(50, WIDTH -100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
@@ -216,12 +412,15 @@ def main():
 			player.x -= player_vel
 		if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: #right
 			player.x += player_vel
-		if keys[pygame.K_w] and player.y + player_vel > 0: #update
+		if keys[pygame.K_w] and player.y + player_vel > 0: #up
 			player.y -= player_vel
 		if keys[pygame.K_s] and player.y + player_vel + player.get_height() + 15 < HEIGHT: #down
 			player.y += player_vel
 		if keys[pygame.K_SPACE]:
 			player.shoot()
+			pygame.mixer.Sound.play(shipShoot)
+		if keys[pygame.K_p]:
+			pause()
 
 		for enemy in enemies[:]:
 			enemy.move(enemy_vel)
@@ -234,25 +433,10 @@ def main():
 				player.health -= 10
 				enemies.remove(enemy)	
 			elif enemy.y  + enemy.get_height() > HEIGHT:
-				lives -= 1
+				player.health -= 10
 				enemies.remove(enemy)
 
 		player.move_lasers(-laser_vel, enemies)
 
-def main_menu():
-	title_font = pygame.font.SysFont("comicsans", 70)
-	run = True
-	while run:
-		WIN.blit(BG, (0,0))
-		title_label = title_font.render("Press the mouse to begin...", 1, (255,255,255))
-		WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
-
-		pygame.display.update()
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				main()
-	pygame.quit()
 
 main_menu()
